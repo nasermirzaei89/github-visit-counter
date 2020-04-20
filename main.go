91118fe26e9afe9ce068b5936fa9c72d6c054779
Naser Mirzaei <nasermirzaei89@gmail.com>
2020-04-20 23:10:03 +0430

    add env params

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"github.com/pkg/errors"
 	bolt "go.etcd.io/bbolt"
 	"net/http"
+	"os"
 )
 
 func main() {
-	db, err := bolt.Open("db.bolt", 0666, nil)
+	db, err := bolt.Open(env("DB_PATH", "db.bolt"), 0666, nil)
 	if err != nil {
 		panic(errors.Wrap(err, "error on open db"))
 	}
@@ -24,8 +25,17 @@ func main() {
 		panic(errors.Wrap(err, "error on update transaction"))
 	}
 
-	err = http.ListenAndServe(":9898", NewHandler(db))
+	err = http.ListenAndServe(env("API_ADDRESS", ":80"), NewHandler(db))
 	if err != nil {
 		panic(errors.Wrap(err, "error on listen and serve"))
 	}
+}
+
+func env(key, def string) string {
+	res, ok := os.LookupEnv(key)
+	if ok {
+		return res
+	}
+
+	return def
 }
